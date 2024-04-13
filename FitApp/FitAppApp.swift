b41ec71a4c3be4a0a4429591ea26ab3a6414866e
MaurZac <maur.mx@gmail.com>
2024-04-13 13:35:24 -0600

    Initial Commit

---
+++ FitApp/FitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAppApp.swift
+//  FitApp
+//
+//  Created by Mauricio Zarate Chula on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
